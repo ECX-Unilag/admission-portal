bad9a66f5e9b8e99195150ab69f40db6b9f2d10c
Mustapha Moshood <olawale3415@gmail.com>
2020-08-16 14:41:15 +0100

    faculty created.

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('course', 'courseController');
 Route::resource('department', 'DepartmentController');
 Route::resource('sessions', 'SessionController');
+Route::resource('faculty', 'FacultyController');
