19c07060dd759d82b2a3704a5c1c0978448150c9
IamSolaSholzy01 <ogolagunju@gmail.com>
2020-08-16 20:17:08 +0100

    Added front end admin-login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,99 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <title>Admission</title>
-        <meta content="Responsive admin theme build on top of Bootstrap 4" name="description" />
-        <meta content="Themesdesign" name="author" />
-        <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets/css/login.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets/css/header-nav.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Admissions Portal | Login</title>
+</head>
 
-        <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-        <link href="{{asset('assets/css/metismenu.min.css')}}" rel="stylesheet" type="text/css">
-        <link href="{{asset('assets/css/icons.css')}}" rel="stylesheet" type="text/css">
-        <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" type="text/css">
-
-    </head>
-
-    <body>
-
-        <!-- Begin page -->
-        <div class="accountbg"></div>
-        <div class="home-btn d-none d-sm-block">
-                <a href="index.html" class="text-white"><i class="fas fa-home h2"></i></a>
+<body>
+    <header>
+        <nav>
+            <a href="index.html"><img class="logo" src="{{asset('assets/images/brand (2).png')}}" alt="ECX Logo"></a>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="create-profile.html">Create Profile</a></li>
+                <li><a href="./contact.html">Contact Us</a></li>
+            </ul>
+            <ul>
+                <li><a class="active" href="./login.html">Login</a></li>
+                <li><a class="sign-up-btn" href="./sign-up.html">Sign Up</a></li>
+            </ul>
+            <div id="openButton" class="mobile-nav-hamburger display-none">
+                <div class="line-1"></div>
+                <div class="line-2"></div>
+                <div class="line-3"></div>
             </div>
-        <div class="wrapper-page">
-                <div class="card card-pages shadow-none">
-    
-                    <div class="card-body">
-                        <div class="text-center m-t-0 m-b-15">
-                                <!-- <a href="index.html" class="logo logo-admin"><img src="{{asset('assets/images/logo-dark.png')}}" alt="" height="24"></a> -->
-                        </div>
-                        <h5 class="font-18 text-center">Sign in Admission Portal.</h5>
-    
-                        <form class="form-horizontal m-t-30" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-12 control-label">E-Mail Address</label>
-
-                            <div class="col-md-12">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-12">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                            <div class="form-group text-center m-t-20">
-                                <div class="col-12">
-                                    <button class="btn btn-primary btn-block btn-lg waves-effect waves-light" type="submit">Log In</button>
-                                </div>
-                            </div>
-    
-                            <!-- <div class="form-group row m-t-30 m-b-0">
-                                <div class="col-sm-7">
-                                    <a href="pages-recoverpw.html" class="text-muted"><i class="fa fa-lock m-r-5"></i> Forgot your password?</a>
-                                </div>
-                                <div class="col-sm-5 text-right">
-                                    <a href="pages-register.html" class="text-muted">Create an account</a>
-                                </div>
-                            </div> -->
-                        </form>
-                    </div>
-    
+            <div id="myModal" class="mobile-nav">
+                <div>
+                    <span style="cursor: pointer;" id="closeBtn" class="close">&times;</span>
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="create-profile.html">Create Profile</a></li>
+                        <li><a href="./contact.html">Contact Us</a></li>
+                        <li><a href="./login.html">Login</a></li>
+                        <li><a href="./sign-up.html">Sign Up</a></li>
+                    </ul>
+                    <a style="text-align: center;" href="index.html"><img
+                            src="{{asset('assets/images/brand (2).png')}}"></a>
                 </div>
             </div>
-        <!-- END wrapper -->
+        </nav>
+    </header>
 
-        <!-- jQuery  -->
-        <script src="{{asset('assets/js/jquery.min.js')}}"></script>
-        <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{asset('assets/js/metismenu.min.js')}}"></script>
-        <script src="{{asset('assets/js/jquery.slimscroll.js')}}"></script>
-        <script src="{{asset('assets/js/waves.min.js')}}"></script>
+    <main class="container col-lg-5 col-xl-4 col-sm-8 col-md-6">
+        <h1>Admin Login</h1>
+        <form class="form-horizontal m-t-30" method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="col-md-12 control-label">E-Mail Address</label>
+                <div class="col-md-12">
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password" class="col-md-4 control-label">Password</label>
+                <div class="col-md-12">
+                    <input id="password" type="password" class="form-control" name="password" required>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+            <button class="btn btn-primary btn-block btn-lg waves-effect waves-light" type="submit">Log In</button>
+        </form>
+        <div class="other-details">
+            <a href="./forgotpassword.html">Forgot Password</a>
+            <span>Don't have an account <a href="./sign-up.html">Sign Up</a></span>
+        </div>
+    </main>
 
-        <!-- App js -->
-        <script src="{{asset('assets/js/app.js')}}"></script>
-        
-    </body>
+    <!--<footer class="mr-0">
+        <a href="./admin-login.html">Admin Login</a>
+    </footer>-->
+    <script src="{{asset('assets/js/main.js')}}"></script>
+</body>
 
 </html>
